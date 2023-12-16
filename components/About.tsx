47a8894894867a213e2b44ffae8089070c6c83ab
matteoaricci <matteo.a.ricci@gmail.com>
2023-12-16 16:51:51 -0500

    minor change

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <section className="h-[calc(100vh-4rem)] px-5 pt-5">
+    <section id="about-section" className="h-[calc(100vh-4rem)] px-5 pt-5">
       <h2 className="text-5xl font-bold pt-8">
         Matteo Ricci is a Frontend Web Developer
       </h2>
