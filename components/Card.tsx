e3610a2bd83853e7793db4093c5febed4bed282d
matteoaricci <matteo.a.ricci@gmail.com>
2024-01-13 12:59:41 -0500

    added animations and card to projects

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,20 +1,56 @@
-export default function Card() {
+"use client";
+
+type CardProps = {
+  title: string;
+  description: string;
+  image?: string;
+  direction: "left" | "right";
+};
+import { motion, useInView, useScroll, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+export default function Card(props: CardProps) {
+  const { title, description, image, direction } = props;
+  const startingPosition = direction === "right" ? "100%" : "-100%";
+  const motionRef = useRef(null);
+  const isInView = useInView(motionRef, { once: true });
+  const controls = useAnimation();
+  const { scrollYProgress } = useScroll();
+  const wrapperVariants = {
+    initial: {
+      translateX: startingPosition,
+    },
+    animate: {
+      translateX: "0%",
+      transition: {
+        delay: 0.25,
+        duration: 0.5,
+      },
+    },
+  };
+
+  useEffect(() => {
+    if (isInView) {
+      controls.start("animate");
+    }
+  }, [isInView, motionRef.current]);
+
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <motion.li
+      ref={motionRef}
+      variants={wrapperVariants}
+      initial="initial"
+      animate={controls}
+      className="max-w-sm bg-white border-2 border-gray-400 rounded-lg shadow my-6"
+    >
       <div className="p-5">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Title
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+          {title}
         </h5>
 
         <div>Image goes Here</div>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Description
-        </p>
-        {/* <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-
-        </a> */}
+        <p className="mb-3 font-normal text-gray-700 ">{description}</p>
       </div>
-    </div>
+    </motion.li>
   );
 }
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,15 +1,57 @@
-import Card from "@/components/Card"
+"use client";
+
+import { useEffect, useRef } from "react";
+import { useInView } from "framer-motion";
+
+import Card from "@/components/Card";
 
 export default function Projects() {
+  const wrapperRef = useRef(null);
+  const isInView = useInView(wrapperRef);
+
+  // useEffect(() => {
+  //   console.log(isInView);
+
+  //   return () => {};
+  // }, [isInView]);
+
   return (
     <section id="projects-section" className="px-5 pt-5">
       <h1 className="text-5xl font-bold pt-8 mb-4">Projects.</h1>
-      <Card />
+      <ul>
+        {projectData.map((proj, index) => {
+          return (
+            <Card
+              {...proj}
+              key={`proejct-${index}`}
+              direction={index % 2 === 0 ? "left" : "right"}
+            />
+          );
+        })}
+      </ul>
     </section>
   );
 }
 
-
-const projectData: {title: string, description: string, image?: string}[] = [
-    
-]
+const projectData: { title: string; description: string; image?: string }[] = [
+  {
+    title: "Close Knit",
+    description:
+      "A social media application with a focus on textiles. Allows user to create communites, show off work, get advice, and more.",
+  },
+  {
+    title: "Close Knit",
+    description:
+      "A social media application with a focus on textiles. Allows user to create communites, show off work, get advice, and more.",
+  },
+  {
+    title: "Close Knit",
+    description:
+      "A social media application with a focus on textiles. Allows user to create communites, show off work, get advice, and more.",
+  },
+  {
+    title: "Close Knit",
+    description:
+      "A social media application with a focus on textiles. Allows user to create communites, show off work, get advice, and more.",
+  },
+];
